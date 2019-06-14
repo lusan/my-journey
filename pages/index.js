e76b34a3d4c3b415ef968733701ddce00da87424
lusan <daslusan@gmail.com>
2019-06-14 22:44:32 +0530

    added prettier

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Header from '../src/components/Header';
-import Footer from '../src/components/Footer';
-import articleData from '../src/articlesData';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import articleData from "../src/articlesData";
 
 const useStyles = makeStyles(theme => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(8, 0, 6)
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(4)
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   cardContent: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function Album() {
@@ -49,21 +49,42 @@ export default function Album() {
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Hi, I am Lusan Das
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              I am a Frontend Engineer with love for Javascript, React.js, PWA and various other web technologies that help in making the web beautiful and engaging.
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              I am a Frontend Engineer with love for Javascript, React.js, PWA
+              and various other web technologies that help in making the web
+              beautiful and engaging.
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
-                  <Button variant="contained" color="primary" href="https://twitter.com/daslusan">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    href="https://twitter.com/daslusan"
+                  >
                     Twitter
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button variant="outlined" color="primary" href="https://www.linkedin.com/in/lusandas">
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    href="https://www.linkedin.com/in/lusandas"
+                  >
                     Linkedin
                   </Button>
                 </Grid>
@@ -89,9 +110,7 @@ export default function Album() {
                     <Typography gutterBottom variant="h5" component="h2">
                       {card.heading}
                     </Typography>
-                    <Typography>
-                      {card.content}
-                    </Typography>
+                    <Typography>{card.content}</Typography>
                   </CardContent>
                   <CardActions>
                     <a href={card.link} title={card.heading} target="_blank">
